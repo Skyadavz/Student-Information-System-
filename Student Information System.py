3b566e3601ffbc2bbe96bc826eb8fb12104694f1
Subhash Kumar yadav <141063498+SkyPy777@users.noreply.github.com>
2023-11-21 01:27:07 +0530

    Add files via upload

---
+++ Student Information System.py
@@ -0,0 +1,22 @@
+
+# Student details
+student_name = "Smith"
+student_age = 20
+
+# Grades
+math_grade = 85
+english_grade = 90
+history_grade = 78
+
+# Courses
+enrolled_courses = ["Mathematics", "English", "History"]
+
+# print student information
+print("Student Information")
+print("Name:", student_name)
+print("Age:", student_age)
+print("Grades:")
+print("  - Mathematics:", math_grade)
+print("  - English:", english_grade)
+print("  - History:", history_grade)
+print("Enrolled Courses:", enrolled_courses)
